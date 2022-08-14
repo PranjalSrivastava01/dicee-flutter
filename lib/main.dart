d87c0b0d22dcd3323cc923fb69f4f75721604f38
PRANJAL <pranjals0105@gmail.com>
2022-08-14 17:28:41 +0530

    dicee-flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.lightBlue,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.lightBlueAccent,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,97 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int P=1;
+  int R=1;
+  int O=1;
+  int B=1;
+  void call(){
+    setState(() {
+      P=Random().nextInt(6)+1;
+      R=Random().nextInt(6)+1;
+      O=Random().nextInt(6)+1;
+      B=Random().nextInt(6)+1;
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return  Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children:<Widget>[
+
+                Expanded(
+                  child: FlatButton(
+                    onPressed: (){
+                      call();
+
+                    },
+                    child: Image(
+                      image: AssetImage('images/dice$P.png'),
+                    ),
+                  ),
+
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: (){
+                      call();
+                    },
+                    child: Image(
+                      image: AssetImage('images/dice$R.png'),
+                    ),
+                  ),
+
+                ),
+              ],
+            ),
+          ),
+  SizedBox(height:30),
+          Expanded(
+            child: Row(
+              children:<Widget>[
+
+                Expanded(
+                  child: FlatButton(
+                    onPressed: (){
+                      call();
+
+                    },
+                    child: Image(
+                      image: AssetImage('images/dice$O.png'),
+                    ),
+                  ),
+
+                ),
+
+                Expanded(
+                  child: FlatButton(
+                    onPressed: (){
+                      call();
+                    },
+                    child: Image(
+                      image: AssetImage('images/dice$B.png'),
+                    ),
+                  ),
+
+                ),
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
